28f47b43cf2210067cf15d9a0be2946b7aa5e4c1
Lukman Memon <52639954+lukmanmemon@users.noreply.github.com>
2021-01-19 19:59:11 -0500

    Added scrollable list and total global data

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
 var map;
-var countriesLocation = [];
+var countriesDetails = [];
+var sortedCountriesCases = [];
+var sortedCountriesDeaths = [];
 const myLatLang = { lat: 56.130, lng: -106.347 };
 
 fetch("https://disease.sh/v3/covid-19/countries")
    .then(response => response.json())
    .then(data => {
+      var countriesCases = [];
+      var countriesDeaths = [];
+
       for (var x = 0; x < data.length; x++) {
-         countriesLocation.push([data[x].country, data[x].countryInfo.lat, data[x].countryInfo.long, data[x].cases, data[x].deaths]);
+         countriesDetails.push([data[x].country, data[x].countryInfo.lat, data[x].countryInfo.long, data[x].cases, data[x].deaths]);
+         countriesCases.push(countriesDetails[x][3] + " " + countriesDetails[x][0]);
+         countriesDeaths.push(countriesDetails[x][4] + " " + countriesDetails[x][0]);
       }
+
+      var newArray = [];
+      var newArray2 = [];
+      for (var i = 0; i < countriesCases.length; i++) {
+        s = countriesCases[i].split(/(?<=^\S+)\s/);
+        newArray.push(s);
+      }
+      newArray.sort(([a, b], [c, d]) => c - a);
+  
+      
+      for (var j = 0; j < newArray.length; j++) {
+        sortedCountriesCases.push(newArray[j][0] + " " + newArray[j][1]);
+      }
+
+      for (var i = 0; i < countriesDeaths.length; i++) {
+        s = countriesDeaths[i].split(/(?<=^\S+)\s/);
+        newArray2.push(s);
+      }
+      newArray2.sort(([a, b], [c, d]) => c - a);
+  
+      
+      for (var j = 0; j < newArray2.length; j++) {
+        sortedCountriesDeaths.push(newArray2[j][0] + " " + newArray2[j][1]);
+      }
+
+      document.getElementById("countriesByCases").appendChild(createList(sortedCountriesCases));
+      document.getElementById("deathsByCases").appendChild(createList(sortedCountriesDeaths));
 });
 
 function initMap() {
@@ -16,19 +50,22 @@ function initMap() {
     zoom: 3,
     minZoom: 1
   });
+
+  var iconBase = "http://maps.google.com/mapfiles/kml/pal3/";
   
   var infowindow = new google.maps.InfoWindow( {
     maxWidth: 100
   });
 
-  for (var i = 0; i < countriesLocation.length; i++) {
+  for (var i = 0; i < countriesDetails.length; i++) {
     marker = new google.maps.Marker({
-        position: new google.maps.LatLng(countriesLocation[i][1], countriesLocation[i][2]),
-        map: map
+        position: new google.maps.LatLng(countriesDetails[i][1], countriesDetails[i][2]),
+        map: map, 
+        icon: iconBase + 'icon38.png'
     });
     marker.addListener('click', (function(marker, i) {
       return function() {
-        infowindow.setContent(countriesLocation[i][0] + "\nCases: " + countriesLocation[i][3] + "\nDeaths: " + countriesLocation[i][4]);
+        infowindow.setContent(countriesDetails[i][0] + "\nCases: " + countriesDetails[i][3] + "\nDeaths: " + countriesDetails[i][4]);
         infowindow.open(map, marker);
       }
     })(marker, i));
@@ -59,3 +96,17 @@ function getTotalGlobalDeaths() {
 
 getTotalGlobalCases();
 getTotalGlobalDeaths();
+
+function createList(arr) {
+  var list = document.createElement("ul");
+  for (var i = 0; i < arr.length; i++) {
+    // Create list item
+    var item = document.createElement("li");
+    // Set contents of item
+    item.appendChild(document.createTextNode(arr[i]));
+    // Add it to the list
+    list.appendChild(item);
+  }
+  return list;
+}
+
